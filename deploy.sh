d5cfa47b768eb4922839ae705a32dbc5a1ae1364
Soller <esolergonzalez@gmail.com>
2024-02-16 00:27:04 -0600

    config deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-!/usr/bin/env sh
 
 # abort on errors
 set -e
@@ -6,20 +5,6 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+git add dist && git commit -m "initial deploy"
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Soller211/todolist-soller.git main:gh-pages
-
-cd -
+git subtree add push --prefix dist origin gh-pages
