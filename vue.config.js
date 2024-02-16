a82c5fd9c8a5daeece07c52bc9ec2a80afd5f8cf
Soller <esolergonzalez@gmail.com>
2024-02-16 00:02:05 -0600

    changes to deploying in github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/todolist-soller/"
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
 })
